69671192cdeb9af6bf44735706e5726077cf7066
nyo <billy31210@gmail.com>
2017-12-28 18:09:40 +0800

    fetch rate data when server start

--- server/server.go
+++ server/server.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 
 	pb "github.com/nyogjtrc/exchange"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 var (
@@ -17,15 +21,51 @@ var (
 // ExchangeServer to implement grpc service
 type ExchangeServer struct {
 	sourceURL string
+	RateMap   RateMap
 }
 
 func (s *ExchangeServer) GetRate(ctx context.Context, in *pb.RateRequest) (*pb.RateReply, error) {
-	log.Println(in)
+	key := in.Base + in.Target
+	rateData, ok := s.RateMap[key]
+	if !ok {
+		return nil, grpc.Errorf(codes.OutOfRange, "currency data not found")
+	}
+
 	return &pb.RateReply{
 		Base:   in.Base,
 		Target: in.Target,
-		Rate:   1.0,
+		Rate:   rateData.Exrate,
 	}, nil
+}
+
+type Rate struct {
+	UTC    string
+	Exrate float64
+}
+
+type RateMap map[string]Rate
+
+func FetchRateAPI() RateMap {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://tw.rter.info/capi.php", nil)
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := make(RateMap)
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return m
 }
 
 func main() {
@@ -34,7 +74,12 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	exServer := &ExchangeServer{
+		sourceURL: sourceURL,
+		RateMap:   FetchRateAPI(),
+	}
+
 	s := grpc.NewServer()
-	pb.RegisterExchangeServiceServer(s, &ExchangeServer{sourceURL})
+	pb.RegisterExchangeServiceServer(s, exServer)
 	s.Serve(lis)
 }
--- server/server_test.go
+++ server/server_test.go
@@ -9,19 +9,43 @@ import (
 )
 
 func TestGetRate(t *testing.T) {
-	s := ExchangeServer{}
-
-	req := &pb.RateRequest{}
-	expectReply := &pb.RateReply{
-		Base:   req.Base,
-		Target: req.Target,
-		Rate:   1.0,
+	m := RateMap{
+		"USDTWD": Rate{
+			UTC:    "2017-12-28 09:12:40",
+			Exrate: 29.792,
+		},
 	}
 
-	rep, err := s.GetRate(context.Background(), req)
-	if err != nil {
-		t.Error(err)
+	s := ExchangeServer{
+		RateMap: m,
 	}
 
-	assert.Equal(t, expectReply, rep)
+	t.Run("USD TWD", func(t *testing.T) {
+		req := &pb.RateRequest{
+			Base:   "USD",
+			Target: "TWD",
+		}
+		expectReply := &pb.RateReply{
+			Base:   req.Base,
+			Target: req.Target,
+			Rate:   29.792,
+		}
+
+		rep, err := s.GetRate(context.Background(), req)
+		if err != nil {
+			t.Error(err)
+		}
+
+		assert.Equal(t, expectReply, rep)
+	})
+
+	t.Run("not found currency", func(t *testing.T) {
+		req := &pb.RateRequest{
+			Base:   "USD",
+			Target: "CYN",
+		}
+
+		_, err := s.GetRate(context.Background(), req)
+		assert.Error(t, err)
+	})
 }
